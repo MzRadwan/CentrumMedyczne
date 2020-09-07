fa426189de67e8a2d93a4783b6334f061d0b536d
MzRadwan <radwan.dominika@gmail.com>
2020-09-07 14:56:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/centrummedyczne/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.centrummedyczne;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "MainActivity";
+    TextView mText1;
+    Button mAddButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mText1 = (TextView) findViewById(R.id.text1);
+        mAddButton =(Button) findViewById(R.id.addButton);
+
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String,Object> address = new HashMap<>();
+                address.put("apartment","15");
+                address.put("building_number", "4b");
+                address.put("city","Lublin");
+                address.put("postal_code","21-212");
+                address.put("street","Makowa");
+
+
+                db.collection("address")
+                        .add(address)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "added, ID = " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG,"Error " ,e);
+                            }
+                        });
+            }
+        });
+
+
+       /* db.collection("specialization")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                mText1.setText(document.getData().toString());
+                            }
+                        }
+                        else{
+                            Log.w(TAG, "Error: ", task.getException());
+                        }
+                    }
+                });
+
+        */
+
+        //CollectionReference addressesRef = db.collection("address");
+      //  Query query = addressesRef.whereEqualTo("city","Lublin");
+
+        db.collection("address")
+                .whereEqualTo("city","Lublin")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                mText1.setText(document.getId() + document.getData().toString());
+                            }
+                        }
+                        else {
+                            mText1.setText("Error: " + task.getException());
+                        }
+                    }
+                });
+
+    }
+}
