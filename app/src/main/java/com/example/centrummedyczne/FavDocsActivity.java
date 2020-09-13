5b6fc988e73cb0fdd8261cc988be79fbd0cee104
MzRadwan <radwan.dominika@gmail.com>
2020-09-13 09:13:38 +0200

    Home button added

--- app/src/main/java/com/example/centrummedyczne/FavDocsActivity.java
+++ app/src/main/java/com/example/centrummedyczne/FavDocsActivity.java
@@ -2,12 +2,16 @@ package com.example.centrummedyczne;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class FavDocsActivity extends AppCompatActivity {
+    ImageView mHomepageImg, mAccountImg;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fav_docs);
@@ -20,5 +24,27 @@ public class FavDocsActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
+
+        mAccountImg = (ImageView) findViewById(R.id.accountImage);
+        mHomepageImg = (ImageView) findViewById(R.id.homepageImage);
+
+        mAccountImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),PatientAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mHomepageImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),PatientAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
