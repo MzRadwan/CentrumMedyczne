b12454f2715d94d4bcc46293886fe2b0db66b7fa
MzRadwan <radwan.dominika@gmail.com>
2020-10-08 17:03:29 +0200

    start_screen_finished

--- app/src/main/java/com/example/centrummedyczne/LoginActivity.java
+++ app/src/main/java/com/example/centrummedyczne/LoginActivity.java
@@ -30,14 +30,14 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //Full Screen Activity
-        this.getWindow().getDecorView().setSystemUiVisibility(
+       /* this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
-
+*/
         mAuth = FirebaseAuth.getInstance();
 
         firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
--- app/src/main/java/com/example/centrummedyczne/WelcomeActivity.java
+++ app/src/main/java/com/example/centrummedyczne/WelcomeActivity.java
@@ -2,13 +2,31 @@ package com.example.centrummedyczne;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button loginButton, continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        loginButton = (Button) findViewById(R.id.loginStart);
+        continueButton = (Button) findViewById(R.id.guestStart);
+    }
+
+    public void onClickLogin(View view){
+        Intent intent = new Intent(view.getContext(),LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickContinue(View view){
+        Intent intent = new Intent(view.getContext(),StartActivity.class);
+        startActivity(intent);
     }
 }
